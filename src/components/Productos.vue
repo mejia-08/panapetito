cf5b4a6a3dbb3b7163741be05ec9cbeb3fc8b25e
sergio-mejia <albansergio65@gmail.com>
2024-06-19 14:42:46 -0500

    etilos ccs al componente de productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -36,3 +36,53 @@ watchEffect(() => {
     productos.value = listaProductos.filter((producto) => producto.categoria === props.categoria );
 });
 </script>
+
+<style scoped>
+    .contenedor{
+        padding: 3rem;
+        margin-bottom: 2rem;
+}
+    .productos{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 2em;
+}
+    .producto{
+        display: flex;
+        align-items: center;
+    }
+    .informacion{
+        padding: 15px;
+    }
+    .titulo{
+        font-size: 1.25rem;
+    }
+    .descripcion{
+        color: gray;
+        word-wrap: break-word;
+    }
+    .foto{
+        max-width: calc(100% / 4);
+        border-radius: 20px;
+        box-shadow: 0px 2px 4px rgb(77, 77, 77);
+    }
+    .precio{
+        color: #f9a72b;
+        font-weight: bold;
+        font-size: 1.25rem;
+    }
+
+    @media (max-width: 1024px) and (min-width: 768px) {
+        .productos{
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+
+    @media (max-width: 768px){
+        .productos{
+            grid-template-columns: repeat(1, 1fr);
+        }
+        
+    }
+</style>
+
