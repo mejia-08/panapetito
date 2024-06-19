6cba6d63b982b9704ad0336c963b931d96c63c92
sergio-mejia <albansergio65@gmail.com>
2024-06-19 12:51:00 -0500

    hicimos el componente titulo para las categorias

---
+++ src/components/TituloSeccion.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+    titulo: String
+})
+</script>
+
+<template>
+    <h1>
+        {{ titulo }}
+    </h1>
+</template>
+
+<style scoped>
+
+h1 {
+    font-size: 2rem;
+    padding: 1rem;
+    text-transform: capitalize;
+}
+
+@media (max-width:768px) {
+    h1 {
+        text-align: center;
+        font-size: 2.5rem;
+        color: #f68712;
+        
+    }
+}
+
+</style>
