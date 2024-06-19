8627f4e9d9437cf90584619da044705680e43518
sergio-mejia <albansergio65@gmail.com>
2024-06-19 14:10:00 -0500

    error de codigo

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      redirect:'/pan',
     },
     {
       path: '/:categoria',
       name: 'productos',
       component: () => import('@/views/ProductosView.vue'),
       props:true
-    }
-  ]
-})
+    },
+  ],
+});
 
-export default router
+export default router;
